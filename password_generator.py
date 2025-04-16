cccfc58a15d7eb44a6dd355e843afedd74dd849d
lilya_todysheva_work <l.todysheva@eipc.center>
2025-04-16 16:23:35 +0300

    Начало работы со словарями. Переписаны первые функции.

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,14 @@
 import random
+import string
 
-digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-punctuation = ['!', '#', '$', '%', '&', '*', '+', '-', '=', '?', '@', '^', '_', '.']
+
+symbol_groups = {
+    'цифры': string.digits,
+    'строчные буквы': string.ascii_lowercase,
+    'прописные буквы': string.ascii_uppercase,
+    'знаки пунктуации': string.punctuation
+}
+ambiguous = 'il1Lo0O'  # сомнительные символы
 
 # проверяем, что пользователь вводит целое число (количество и длина паролей)
 def get_int_input(prompt):
@@ -13,39 +18,36 @@ def get_int_input(prompt):
         except ValueError:
             print("Ошибка! Пожалуйста, введите число.")
 
+
 # проверка ответов пользователя "да" и "нет"
 def get_yes_no_input(prompt):
     while True:
-        answer = input(prompt).lower()
-        if answer in ['да', 'lf', 'yes', 'нет', 'ytn', 'no']:
-            return answer
+        answer = input(prompt).strip().lower()
+        if answer in ['да', 'lf', 'yes', '+']:
+            return True
+        elif answer in ['нет', 'ytn', 'no', '-']:
+            return False
         else:
             print("Некорректный ответ! Введите 'да' или 'нет'.")
 
-# составляем список символов для генерации пароля
-def password_chars():
-    chars = []
 
-    if get_yes_no_input('Включать ли в пароль цифры от 0 до 9? ') in ['да', 'lf', 'yes']:
-        chars += digits
+# собираем настройки от пользователя для символов в пароле
+def get_user_settings():
+    selected_groups = dict()
 
-    if get_yes_no_input('Включать ли в пароль прописные буквы? ') in ['да', 'lf', 'yes']:
-        chars += uppercase_letters
+    for key, value in symbol_groups.items():
+        include = get_yes_no_input(f"Включать ли в пароль {key}?")
+        if include:
+            selected_groups[key] = value
 
-    if get_yes_no_input('Включать ли в пароль строчные буквы? ') in ['да', 'lf', 'yes']:
-        chars += lowercase_letters
+    if not selected_groups:
+        print("Вы не выбрали ни одну группу символов. Пожалуйста, выберите хотя бы одну.")
+        return get_user_settings()
 
-    if get_yes_no_input('Включать ли в пароль символы "!#$%&*+-=?@^_."? ') in ['да', 'lf', 'yes']:
-        chars += punctuation
+    exclude_ambiguous = get_yes_no_input(f"Исключить ли неоднозначные символы {ambiguous}?")
 
-    if get_yes_no_input('Исключать ли неоднозначные символы "il1Lo0O"? ') in ['да', 'lf', 'yes']:
-        chars = [ch for ch in chars if ch not in "il1Lo0O"]
+    return selected_groups, exclude_ambiguous
 
-    if not chars:
-        print("Ошибка: Не выбраны символы для пароля. Пожалуйста, выберите хотя бы одну категорию символов.")
-        return password_chars()
-
-    return chars
 
 # генерируем пароль (из каждой группы берем минимум 1 символ, оставшиеся символы дополняем случайными из набора, перемешиваем)
 def generate_password(length, chars):
@@ -69,21 +71,24 @@ def generate_password(length, chars):
     random.shuffle(password)
     return ''.join(password)
 
+
 # запускаем программу
 def main():
     pwd_quantity = get_int_input('Сколько паролей вам нужно сгенерировать? Введите число. ')
 
     while True:
-        pwd_len = get_int_input(
+        pwd_length = get_int_input(
             'Какой длины должен быть пароль? Введите число, пароль должен включать минимум 8 символов, максимум 20 символов. ')
-        if 8 <= pwd_len <= 20:
+        if 8 <= pwd_length <= 20:
             break
         else:
             print('Некорректный ввод - пароль должен включать минимум 8 символов, максимум 20 символов. Введите число. ')
 
-    chars = password_chars()
-    for i in range(pwd_quantity):
-        print(generate_password(pwd_len, chars))
+    selected_groups, exclude_ambiguous = get_user_settings()
+    char_pool = prepare_character_pool(selected_groups, exclude_ambiguous)
+
+    for _ in range(pwd_quantity):
+        print(generate_password(pwd_length, char_pool))
 
 
 main()
